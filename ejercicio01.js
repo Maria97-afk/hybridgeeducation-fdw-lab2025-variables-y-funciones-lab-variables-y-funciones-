8f9052406795e6477c6b43a96c8b58ec2ef71b72
Maria97-afk <mariamarchan97@icloud.com>
2025-11-03 18:56:45 -0600

    Maria Guadalupe Marchan.js

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,12 +1,12 @@
 // Ejercicio 1: Declaración de Función
 // Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
 
-function obtenerNombre() {
+function obtenerNombre(Maria Guadalupe Marchan Huerta) {
     // Escribe tu código aquí
 }
 
 // No modificar.
 // Exporta la función para que pueda ser utilizada en otros archivos
-module.exports = {
+module.exports = {Maria Guadalupe Marchan Huerta
     obtenerNombre
 };
